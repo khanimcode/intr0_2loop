4b2705e8420988eee742ac9b40c28d3c602130e1
pshvaa <sabira.pshva@gmail.com>
2022-10-30 22:39:31 +0400

    created

---
+++ intro_loop.cs
@@ -0,0 +1,136 @@
+using System;
+
+namespace tasks_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //TASK-1: the number is power of 2 or not
+
+
+            //#region 
+            //int num = 100;
+            //if (num > 0)
+            //{
+            //    while (num % 2 == 0)
+            //    {
+            //        num = num / 2;
+            //    }
+            //    if (num == 1)
+            //    {
+            //        Console.WriteLine("Number is power of 2");
+            //    }
+            //    else if(num == 0 || num != 1)
+            //    {
+            //        Console.WriteLine("Number is not power of 2");
+
+            //    }
+            //}
+            //#endregion
+
+
+            // TASK-2:The NUmber is polindrome or not
+
+            //#region 
+            //int num, r, sum = 0;
+            //int t = 0;
+            //Console.WriteLine("input a number: ");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //t = num;
+            //while (num > 0)
+            //{
+            //    r = num % 10;
+            //    sum = sum * 10 + r;
+            //    num = num / 10;
+            //}
+            //if (t == sum)
+            //    Console.WriteLine("This number is  palindrome ");
+            //else
+            //    Console.WriteLine("This number is not  palindrome ");
+            //#endregion
+
+            //TASK-3 
+            #region
+            // int num = 456 ;
+            // if(num != 0 && num != 2)
+            // {
+            //    if (num % 2 == 0)
+            //    {
+
+            //        Console.WriteLine("number is complex");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("number is not complex");
+
+            //    }
+
+            // }
+            #endregion
+
+            //TASK-4
+            #region
+            // int month = 5;
+            // switch (month)
+            // {
+            //     case 1:
+
+            //        Console.WriteLine("winter");
+            //         break;
+            //     case 2:
+
+            //        Console.WriteLine("winter");
+            //         break;
+            //     case 3:
+
+            //         Console.WriteLine("spring");
+            //         break;
+            //     case 4:
+
+            //         Console.WriteLine("spring");
+            //         break;
+            //     case 5:
+
+            //         Console.WriteLine("spring");
+            //         break;
+            //     case 6:
+
+            //         console.log("summer");
+            //         break;
+            //     case 7:
+
+            //         Console.WriteLine("summer");
+            //         break;
+            //     case 8:
+
+            //         Console.WriteLine("summer");
+            //         break;
+            //     case 9:
+
+            //         Console.WriteLine("autumn");
+            //         break;
+            //     case 10:
+
+            //             Console.WriteLine("autumn");
+            //         break;
+            //     case 11:
+            //         Console.WriteLine("autumn");
+            //         break;
+            //     default:
+
+            //         Console.WriteLine("winter");
+            //         break;
+
+            // }     
+
+            #endregion
+
+        }
+    }
+
+    }
+}
---
+++ task_intro2.js
@@ -0,0 +1,113 @@
+ //TASK-1: the number is power of 2 or not
+//  var num = 100;
+//             if (num > 0)
+//             {
+//                while (num % 2 == 0)
+//                {
+//                    num = num / 2;
+//                }
+//                if (num == 1)
+//                {
+//                    console.log("Number is power of 2");
+//                }
+//                else if(num == 0 || num != 1)
+//                {
+//                 console.log("Number is not power of 2");
+
+//                }
+//             }
+
+//
+            // TASK-2:The NUmber is polindrome or not
+
+            // let remainder, temp, sum = 0;
+            // let number = 3445;
+            // temp = number;
+            // do{
+            //     remainder = number%10;
+            //     sum= (sum*10)+remainder;
+            //         number = number/10;
+    
+            // }	while(number!=0)
+            // if(sum == temp)
+            // {
+            // console.log("number is palindrome");
+            // }
+            // else
+            // {
+            // console.log("number is not palindrome");
+            // }
+        
+
+            //TASK-3 
+            // let num = 456 ;
+            // if(num != 0 && num != 2)
+            // {
+            //    if (num % 2 == 0)
+            //    {
+
+            //        console.log("number is complex");
+            //    }
+
+            //    else
+            //    {
+            //        console.log("number is not complex");
+
+            //    }
+
+            // }
+
+//TASK-4
+
+            // let month = 5;
+            // switch (month)
+            // {
+            //     case 1:
+
+            //         console.log("winter");
+            //         break;
+            //     case 2:
+
+            //         console.log("winter");
+            //         break;
+            //     case 3:
+
+            //         console.log("spring");
+            //         break;
+            //     case 4:
+
+            //         console.log("spring");
+            //         break;
+            //     case 5:
+
+            //         console.log("spring");
+            //         break;
+            //     case 6:
+
+            //         console.log("summer");
+            //         break;
+            //     case 7:
+
+            //         console.log("summer");
+            //         break;
+            //     case 8:
+
+            //         console.log("summer");
+            //         break;
+            //     case 9:
+
+            //         console.log("autumn");
+            //         break;
+            //     case 10:
+
+            //             console.log("autumn");
+            //         break;
+            //     case 11:
+            //         console.log("autumn");
+            //         break;
+            //     default:
+
+            //         console.log("winter");
+            //         break;
+
+            // }     
